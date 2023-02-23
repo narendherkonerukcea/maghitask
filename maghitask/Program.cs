460581f1b2f2837111501aa7d614fc419e860d10
koneru narendher <koneru narendher@DESKTOP-OE6V0M0>
2023-02-23 09:40:23 +0530

    Add project files.

---
+++ maghitask/Program.cs
@@ -0,0 +1,133 @@
+﻿//Explaination of single threading:
+
+//In this example, the program prompts the user to enter their name, reads the input from the console using Console.ReadLine(), and then prints a greeting message that includes the user's name. The program then waits for user input again before exiting.
+
+//Since this is a single-threaded program, all the code executes on the same thread, which means that only one operation can be performed at a time. In this case, the program waits for the user to enter their name before continuing to the next line of code. Once the user enters their name, the program proceeds to print the greeting message and then waits for user input again.
+
+//Note that a single-threaded program is suitable for simple tasks that don't require parallel execution, such as reading user input, performing basic calculations, and displaying output to the user. However, for more complex tasks that involve time-consuming operations or processing large amounts of data, a multi-threaded program may be more appropriate to improve performance.
+
+
+
+
+
+//example code for single threading:
+
+
+//using System;
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("Enter your name: ");
+//        string name = Console.ReadLine();
+
+//        Console.WriteLine($"Hello, {name}! This is a single-threaded program.");
+
+//        // Do some work here...
+
+//        Console.ReadLine();
+//    }
+//}
+
+
+
+
+
+//multi threading explaination:
+// to write applications that can perform multiple tasks concurrently.
+
+
+
+//multi threading code:
+
+
+//using System;
+//using System.Threading;
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        // create a new thread
+//        Thread t = new Thread(Worker);
+
+//        // start the thread
+//        t.Start();
+
+//        // do some other work in the main thread
+//        for (int i = 0; i < 10; i++)
+//        {
+//            Console.WriteLine("Main thread doing some work");
+//            Thread.Sleep(100);
+//        }
+
+//        // wait for the worker thread to complete
+//        t.Join();
+
+//        Console.WriteLine("Done");
+//    }
+
+//    static void Worker()
+//    {
+//        for (int i = 0; i < 10; i++)
+//        {
+//            Console.WriteLine("Worker thread doing some work");
+//            Thread.Sleep(100);
+//        }
+//    }
+//}
+
+
+
+
+// Explaination of what to synchronize to threading
+
+
+
+//In this example, MySharedResource represents a shared resource that multiple threads will access concurrently.The counter field is the shared data that needs to be synchronized.
+
+//To ensure that only one thread can access the counter field at a time, we use the Monitor class to create a mutual exclusion block. The lockObject is an object that is used as a lock, and it's used to synchronize access to the counter field in both the IncrementCounter() and GetCounterValue() methods.
+
+//By calling Monitor.Enter(lockObject) before accessing the shared data, we obtain a lock on the lockObject and prevent other threads from accessing the data at the same time. The try-finally block ensures that the lock is always released, even if an exception is thrown.
+
+//Note that this is just one example of how to synchronize access to shared data in C#. There are many other synchronization techniques available, such as using the ReaderWriterLockSlim class or the SemaphoreSlim class, depending on the specific requirements of your application.
+
+
+
+
+
+// code for what to synchronize to threading
+
+    using System;
+class MySharedResource
+{
+    private int counter = 0;
+    private object lockObject = new object();
+
+    public void IncrementCounter()
+    {
+        Monitor.Enter(lockObject);
+        try
+        {
+            counter++;
+        }
+        finally
+        {
+            Monitor.Exit(lockObject);
+        }
+    }
+
+    public int GetCounterValue()
+    {
+        Monitor.Enter(lockObject);
+        try
+        {
+            return counter;
+        }
+        finally
+        {
+            Monitor.Exit(lockObject);
+        }
+    }
+}
